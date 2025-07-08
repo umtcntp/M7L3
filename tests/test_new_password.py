a0f47f572c9fcd3fbcf0fbdcca5c0af235835d66
emocan20221 <cizgikirali@gmail.com>
2025-07-08 22:36:52 +0300

    secando commit

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,6 +8,22 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
+def test_password_length():
+    """Şifrenin uzunluğunun belirtilen uzunlukla eşleşip eşleşmediğini test edin"""
+    for length in range(1,21):
+        assert len(generate_password(length)) == length
+
+def test_password_randomness():
+    password1 = generate_password(10)
+    password2 = generate_password(10)
+    assert password1 != password2
+
+
+
+
+
+
+
 """
 Aşağıda önerilenlerden birini kullanarak başka bir test yazın. Alternatif olarak, kendi testinizi de oluşturabilirsiniz!
 Daha fazla test yazabilirseniz harika olur!
