6b3e71a85dfd52d04b7ba49d1028a1b69865f1b8
hAKKIALKANER1487 <bertugorhan2542@gmail.com>
2025-07-08 22:36:59 +0300

    second commit

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,10 +8,11 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
-"""
-Aşağıda önerilenlerden birini kullanarak başka bir test yazın. Alternatif olarak, kendi testinizi de oluşturabilirsiniz!
-Daha fazla test yazabilirseniz harika olur!
+def test_password_length():
+    for length in range(1, 21): 
+        assert len(generate_password(length)) == length
 
-1. Şifrenin uzunluğunun belirtilen uzunlukla eşleşip eşleşmediğini test edin  
-2. Arka arkaya oluşturulan iki şifrenin farklı olup olmadığını test edin 
-"""
+def test_password_randomness():
+    password1 = generate_password(10)
+    password2 = generate_password(10)
+    assert password1 != password2
